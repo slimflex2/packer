599f8bbb947f200d47e25693b634bdf4517cfe4a
bern <you@example.com>
2020-12-15 09:44:47 -0600

    new

---
+++ files/jenkins.sh
@@ -0,0 +1,8 @@
+#/bin/bash 
+sudo yum install -O /etc/yum.repos.d/jenkins.repo\
+	https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+
+
